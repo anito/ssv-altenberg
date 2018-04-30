262a2a214b463d1d4067c583f0fcea9226aa1e4a
anito <axelnitzschner@mac.com>
2018-04-30 12:18:04 +0200

    H1 team header

--- wp-content/themes/mesmerize-pro-child/sportspress/team-details.php
+++ wp-content/themes/mesmerize-pro-child/sportspress/team-details.php
@@ -16,6 +16,6 @@ $team = new SP_Team( $id );
 $team_name = $team->post->post_title;
 $members = $team->staff();
 $link_staff = get_option( 'sportspress_team_link_staff', 'no' ) === 'yes' ? true : false;
-$header = '<span class="sp-team-name"><h3>Team ' . $team_name . '</h3></span>';
+$header = '<span class="sp-team-name"><h1>Team ' . $team_name . '</h1></span>';
         
 echo $header;
