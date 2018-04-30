4141d20bd055b1c7b0d2a843312c822238583db3
anito <axelnitzschner@mac.com>
2018-04-30 06:16:18 +0200

    Added class page-main-column in sportspress.php as the main sports press page-template

--- wp-content/themes/mesmerize-pro-child/page-templates/homepage.php
+++ wp-content/themes/mesmerize-pro-child/page-templates/homepage.php
@@ -6,7 +6,7 @@ mesmerize_get_header('homepage');
 ?>
 
 <div class="page-content">
-  <div class="<?php mesmerize_page_content_wrapper_class(); ?>">
+  <div class="<?php mesmerize_page_content_wrapper_class(); ?> page-main-column">
    <?php 
       while ( have_posts() ) : the_post();
         the_content();
