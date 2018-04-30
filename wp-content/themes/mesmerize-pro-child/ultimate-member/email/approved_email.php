0ed7bc737e70aea3a41c17fcfd4779af40051f69
anito <axelnitzschner@mac.com>
2018-04-30 03:40:37 +0200

    Ultimate member changes

--- wp-content/themes/mesmerize-pro-child/ultimate-member/email/approved_email.php
+++ wp-content/themes/mesmerize-pro-child/ultimate-member/email/approved_email.php
@@ -4,7 +4,7 @@
 <div style="clear: both;"> </div>
 </div>
 <div style="padding: 0 30px 30px 30px; border-bottom: 3px solid #eeeeee;">
-<div style="padding: 30px 0; font-size: 24px; text-align: center; line-height: 40px;"><span style="display: block;">Dein Konto wurde aktiviert.</span></div>
+<div style="padding: 30px 0; font-size: 24px; text-align: center; line-height: 40px;"><span style="display: block;">Glückwunsch! Dein Konto wurde aktiviert.</span></div>
 <div style="padding: 10px 0 50px 0; text-align: center;"><a style="background: #555555; color: #fff; padding: 12px 30px; text-decoration: none; border-radius: 3px; letter-spacing: 0.3px;" href="{login_url}">Login</a></div>
 <div style="padding: 0 0 15px 0;">
 <div style="background: #eee; color: #444; padding: 12px 15px; border-radius: 3px; font-weight: bold; font-size: 16px;">Konto Informationen</div>
