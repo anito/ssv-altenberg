ed35dce3d47b27f8426b0d913742d3d683ab0475
anito <axelnitzschner@mac.com>
2018-05-07 19:47:24 +0200

    Updated templates to new version sprtspress 2.6

--- wp-content/themes/mesmerize-pro-child/sportspress/player-details.php
+++ wp-content/themes/mesmerize-pro-child/sportspress/player-details.php
@@ -4,7 +4,7 @@
  *
  * @author 		ThemeBoy
  * @package 	SportsPress/Templates
- * @version   2.5
+ * @version   2.6
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
@@ -23,7 +23,6 @@ $defaults = array(
 	'show_leagues' => get_option( 'sportspress_player_show_leagues', 'no' ) == 'yes' ? true : false,
 	'show_seasons' => get_option( 'sportspress_player_show_seasons', 'no' ) == 'yes' ? true : false,
 	'show_nationality_flags' => get_option( 'sportspress_player_show_flags', 'yes' ) == 'yes' ? true : false,
-	'abbreviate_teams' => get_option( 'sportspress_abbreviate_teams', 'yes' ) === 'yes' ? true : false,
 	'link_teams' => get_option( 'sportspress_link_teams', 'no' ) == 'yes' ? true : false,
 );
 
@@ -36,7 +35,6 @@ $player = new SP_Player( $id );
 $metrics_before = $player->metrics( true );
 $metrics_after = $player->metrics( false );
 $metrics_after = array_map('strtoupper', $player->metrics( false ));
-
 
 $common = array();
 
@@ -78,7 +76,7 @@ if ( $show_current_teams ):
 	if ( $current_teams ):
 		$teams = array();
 		foreach ( $current_teams as $team ):
-			$team_name = sp_get_team_name( $team, $abbreviate_teams );
+			$team_name = sp_team_short_name( $team );
 			if ( $link_teams ) $team_name = '<a href="' . get_post_permalink( $team ) . '">' . $team_name . '</a>';
 			$teams[] = $team_name;
 		endforeach;
@@ -91,7 +89,7 @@ if ( $show_past_teams ):
 	if ( $past_teams ):
 		$teams = array();
 		foreach ( $past_teams as $team ):
-			$team_name = sp_get_team_name( $team, $abbreviate_teams );
+			$team_name = sp_team_short_name( $team );
 			if ( $link_teams ) $team_name = '<a href="' . get_post_permalink( $team ) . '">' . $team_name . '</a>';
 			$teams[] = $team_name;
 		endforeach;
