eece57d9403d5202ad2149563df9cecbb5a2e0b2
anito <axelnitzschner@mac.com>
2018-05-26 17:52:08 +0200

    init

---
+++ wp-content/themes/mesmerize-pro-child/scrapbook.php
@@ -0,0 +1,61 @@
+<div data-label="Overlappable" data-id="overlappable-1" data-export-id="overlappable-4" data-category="overlappable" class="overlappable-4 content-section content-section-spacing" data-overlap="true" id="overlappable-1"  style="background-color: rgb(255, 255, 255);"> <div class="gridContainer" > <div data-type="row" class="row spaced-cols text-center" > <div class="col-xs-12 col-sm-4 col-padding col-md-12 " > <i class="fa icon shadow-large-black aligncenter fa-black-tie reverse round large color1" > </i> <div data-type="column" class="" > <h5 class="" ><p >Übersicht über Mitarbeiter unseres Sportvereins</p></h5>  </div> </div>   </div> </div> </div><div  data-label="Team" data-id="team-4" data-export-id="team-2" data-category="team" class="team-2 content-section content-section-spacing" id="team-4" style="background-color: #f5fafd;">
+
+    <div  class="gridContainer">
+
+        <div  class="row text-center">
+
+            <div  class="section-title-col" data-type="column">
+
+                <h2 ><p >Vorsitzender und Abteilungsleiter</p></h2>
+
+
+                <div  data-editable="true" data-name="ope-widgets-area" data-content-shortcode="mesmerize_display_widgets_area id=&quot;mesmerize_users_custom_widgets_areas_0&quot; " class="">[mesmerize_display_widgets_area id="mesmerize_users_custom_widgets_areas_0"]</div>
+
+            </div>
+
+        </div>
+
+    </div>
+</div><div  data-label="Team" data-id="team-2" data-export-id="team-2" data-category="team" class="team-2 content-section content-section-spacing" id="team-2" style="background-color: #f5fafd; background-image: none;">
+
+    <div  class="gridContainer">
+
+        <div  class="row text-center">
+
+            <div  class="section-title-col" data-type="column">
+
+                <h2 >Unser Trainerteam</h2>
+
+
+                <p  class="lead">Für das Training in den jeweiligen Sektionen sind folgende Trainer verantwortlich</p>
+
+
+                <div  data-editable="true" data-name="ope-custom-content-shortcode" data-content-shortcode="staff_gallery id=&quot;819&quot; title=&quot;Trainer&quot; number=&quot;500&quot; show_all_staff_link=&quot;0&quot; align=&quot;none&quot;" class="">[staff_gallery id="819" title="Trainer" number="500" show_all_staff_link="0" align="none"]</div>
+
+            </div>
+
+        </div>
+
+    </div>
+</div><div  data-label="Team" data-id="team-1" data-export-id="team-1" data-category="team" class="team-1 content-section content-section-spacing" id="team-1" style="background-color: #ffffff;">
+
+    <div  class="gridContainer">
+
+        <div  class="row text-center">
+
+            <div  class="section-title-col" data-type="column">
+
+                <h2  class="">Öffentlichkeitsarbeit</h2>
+
+
+                <p  class="lead">Liste der Mitarbeiter für Öffentlichkeitsarbeit</p>
+
+
+                <div  data-editable="true" data-name="ope-widgets-area" data-content-shortcode="mesmerize_display_widgets_area id=&quot;mesmerize_users_custom_widgets_areas_3&quot; " class="">[mesmerize_display_widgets_area id="mesmerize_users_custom_widgets_areas_3"]</div>
+
+            </div>
+
+        </div>
+
+    </div>
+</div>
