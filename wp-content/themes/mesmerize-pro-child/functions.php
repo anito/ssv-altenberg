5396dcb511797ec199035d6192a0a6bce1cf52ef
anito <axelnitzschner@mac.com>
2018-06-21 11:30:11 +0200

    automatically add team content read more link

--- wp-content/themes/mesmerize-pro-child/functions.php
+++ wp-content/themes/mesmerize-pro-child/functions.php
@@ -544,7 +544,7 @@ function add_team_posts_permalink( $content ) {
     $slug = $post->post_name;
     $permalink = home_url( $category_base . '/' .  $slug );
     
-    $output = '<div><a class="read-more" href="' . $permalink . '">alle Sektionsbeiträge lesen</a></div>';
+    $output = '<div><a class="button big color1 y-move" href="' . $permalink . '">alle Sektionsbeiträge lesen</a></div>';
     
     return $content . $output;
     
