71e6b2d4e6aa6582087652f91a55c2e58fc37e53
anito <axelnitzschner@mac.com>
2018-06-26 01:49:36 +0200

    email pending

--- wp-content/themes/mesmerize-pro-child/ultimate-member/email/pending_email.php
+++ wp-content/themes/mesmerize-pro-child/ultimate-member/email/pending_email.php
@@ -3,7 +3,7 @@
 <div style="text-align: center; font-weight: 600; font-size: 26px; padding: 10px 0; border-bottom: solid 3px #eeeeee;">{site_name}</div>
 <div style="clear: both;"> </div>
 </div>
-<div style="padding: 15px; background: #eee; border-radius: 3px; text-align: center;"><span style="font-size: 24px;">Prüfung des Profils</span></div>
+<div style="padding: 15px; background: #eee; border-radius: 3px; text-align: center;"><span style="font-size: 24px;">Prüfung Deines Profils</span></div>
 <div style="padding: 30px 0; font-size: 14px; text-align: center;">
 <p class="p1">Für Dein Profil ist eine Prüfung erforderlich.<br />
 Ein Administrator<span class="Apple-converted-space">  </span>wird Dein Konto in Kürze aktivieren.</p>
@@ -11,6 +11,9 @@ Ein Administrator<span class="Apple-converted-space">  </span>wird Dein Konto i
 <div style="padding: 0 30px 30px 30px; border-bottom: 3px solid #eeeeee;">
 <div style="padding: 15px; background: #eee; border-radius: 3px; text-align: center;"><span style="display: block; font-size: 16px; color: #ff0000;"><strong style="color: #666666; font-size: 15px;">Bitte habe etwas Geduld</strong><br />
 </span></div>
+<div>
+<p style="text-align: center;">Solltest Du Fragen haben, kontaktiere uns bitte per E-Mail {admin_email}</p>
+</div>
 </div>
 <div style="color: #999; padding: 20px 30px;">
 <div>Vielen Dank!</div>
