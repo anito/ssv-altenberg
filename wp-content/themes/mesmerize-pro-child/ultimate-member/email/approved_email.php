59cd8fffdc30c97cb355ea2cefa087429e5b1cbd
anito <axelnitzschner@mac.com>
2018-06-27 23:19:28 +0200

    changes

--- wp-content/themes/mesmerize-pro-child/ultimate-member/email/approved_email.php
+++ wp-content/themes/mesmerize-pro-child/ultimate-member/email/approved_email.php
@@ -4,15 +4,14 @@
 <div style="clear: both;"> </div>
 </div>
 <div style="padding: 0 30px 30px 30px; border-bottom: 3px solid #eeeeee;">
-<div style="padding: 30px 0; font-size: 24px; text-align: center;"><span style="display: block;">Dein Konto wurde aktiviert.</span></div>
-<div style="padding: 10px 0 50px 0; text-align: center;"><a style="background: #555555; color: #fff; padding: 12px 30px; text-decoration: none; border-radius: 3px; letter-spacing: 0.3px;" href="{login_url}">Login</a></div>
+<div style="padding: 30px 0; font-size: 24px; text-align: center;"><span style="display: block;">Dein Konto wurde genehmigt</span></div>
 <div style="padding: 0 0 15px 0;">
-<div style="background: #eee; color: #444; padding: 12px 15px; border-radius: 3px; text-align: center; font-weight: bold; font-size: 16px;">Konto:</div>
 <div style="text-align: center; color: #333;">
-<p style="color: #999;">Deine E-Mail: <strong>{email}</strong></p>
+<p><span style="color: #999999; background-color: #ffffff; font-size: 15px;">Deine E-Mail: </span><strong style="color: #999999; background-color: #ffffff; font-size: 15px;">{email}</strong></p>
 <p style="color: #999;">Dein Benutzername: <strong>{username}</strong></p>
 <p style="color: #999;">Dein Passwort kannst Du <a style="font-weight: bold;" href="{password_reset_link}">hier</a> ändern.</p>
 </div>
+<div style="padding: 30px 0 10px; text-align: center;"><a style="background: #555555; color: #fff; padding: 12px 30px; text-decoration: none; border-radius: 3px; letter-spacing: 0.3px;" href="{login_url}">Login</a></div>
 </div>
 </div>
 <div style="color: #999; padding: 20px 30px;">
