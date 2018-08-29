b4925267812b62802a938cae06abfda47fc25661
anito <axelnitzschner@mac.com>
2018-08-29 13:55:34 +0200

    scrolltop for mobile

--- wp-content/themes/mesmerize-pro-child/js/main.js
+++ wp-content/themes/mesmerize-pro-child/js/main.js
@@ -21,6 +21,10 @@
     
     var add_check_scroll = function() {
             
+            var timer_id;
+            
+            $('body').attr('id', 'the-top');
+            
             $(window).on('scroll', function(e) {
 
                 var cl = '.scroll-top',
@@ -29,8 +33,22 @@
 
                 if( scrollTop > height ) {
                     div_handler( cl );
+                    
+                    $( cl ).removeClass( 'dimmed' );
+                    
+                    clearTimeout( timer_id );
+                    timer_id = setTimeout( function() {
+                        console.log(timer_id)
+                        $( cl ).addClass( 'dimmed' );
+                    }, 2000);
+                    
                 } else {
-                    $( cl ).addClass( 'dimmed' );
+                    $( cl ).addClass( 'away' );
+                    
+                    if( timer_id ) {
+                        clearTimeout(timer_id);
+                        timer_id = null;
+                    }
                 }
 
                 e.preventDefault();
@@ -47,17 +65,16 @@
                 
                 if( !el.length ) {
                     
-                    $('body').attr('id', 'the-top');
 
                     str = cl.replace(/(^\.|\.)/g, " ");
-                    tmpl = `<div class="${str} dimmed"><a href="#the-top" class="scroll-top-inner fa icon fa-angle-up"></a></div>`;
+                    tmpl = `<div class="${str} away"><a href="#the-top" class="scroll-top-inner fa icon fa-angle-up"></a></div>`;
                     
                     $('body').append( $(tmpl) );
                     
                     add_animate_scroll();
                 }
 
-                el.removeClass( 'dimmed' );
+                el.removeClass( 'away' );
             };
 
         }
