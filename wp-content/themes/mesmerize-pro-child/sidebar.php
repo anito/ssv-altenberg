4667f027ef9f0b71620029d0f5a19e577f764392
anito <axelnitzschner@mac.com>
2018-11-10 14:16:50 +0100

    Sidebar class page-sidebar-column added for sidebar.php

--- wp-content/themes/mesmerize-pro-child/sidebar.php
+++ wp-content/themes/mesmerize-pro-child/sidebar.php
@@ -9,31 +9,33 @@ if ( ! is_active_sidebar('sidebar-1') ) {
 }
 ?>
 
-<div class="sidebar col-sm-4 col-md-3">
-    <div class="sidebar-row">
-        <?php
-        dynamic_sidebar( "sidebar-1" );
-        
-        $teams = get_teams();
-            
-        $ssv_terms = get_the_terms($post, SSV_CATEGORY_BASE);
-        $tag_terms = get_the_terms($post, 'post_tag');
+<div class="col-xs-12 col-sm-4 col-md-3 page-sidebar-column">
+    <div class="sidebar">
+        <div class="sidebar-row">
+            <?php
+            dynamic_sidebar( "sidebar-1" );
 
-        $terms = wp_parse_args($ssv_terms, $tag_terms);
-        $slugs = array_map( function( $term ) {
-            return $term->slug;
-        }, $terms );
+            $teams = get_teams();
 
-        $slugs = array_unique($slugs);
+            $ssv_terms = get_the_terms($post, SSV_CATEGORY_BASE);
+            $tag_terms = get_the_terms($post, 'post_tag');
 
-        foreach ( $teams as $team ) {
-            if( in_array( $team->post_name, $slugs ) ) {
-                $team_id = $team->ID;
-                dynamic_sidebar( "sportspress_pages_sidebar_$team_id" );
+            $terms = wp_parse_args($ssv_terms, $tag_terms);
+            $slugs = array_map( function( $term ) {
+                return $term->slug;
+            }, $terms );
+
+            $slugs = array_unique($slugs);
+
+            foreach ( $teams as $team ) {
+                if( in_array( $team->post_name, $slugs ) ) {
+                    $team_id = $team->ID;
+                    dynamic_sidebar( "sportspress_pages_sidebar_$team_id" );
+                }
             }
-        }
-                        
-            
-        ?>
+
+
+            ?>
+        </div>
     </div>
 </div>
